f4b6fc5faf62592f3791c7bb2c13c7c570e8889b
William ´´ <William >
2018-01-13 16:06:17 -0200

    form implemented with validation

---
+++ app/controllers/chat.js
@@ -0,0 +1,16 @@
+module.exports.iniciaChat = function(application, req, res){
+
+	var dadosForm = req.body;
+
+	req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
+	req.assert('apelido', 'Nome ou apelido deve conter entre 2 e 12 caracteres').len(2, 15);
+
+	var erros = req.validationErrors();
+
+	if(erros){
+		res.render('index', {erros : erros})
+		return;
+	}
+
+	res.render('chat');
+}
