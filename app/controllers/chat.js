7dcf63504806e6ce33dc49903acab495ed223f42
william <willshinji@gmail.com>
2018-03-09 22:44:14 -0300

    added field senha for login

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -4,6 +4,7 @@ module.exports.iniciaChat = function(application, req, res){
 
 	req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
 	req.assert('apelido', 'Nome ou apelido deve conter entre 2 e 12 caracteres').len(2, 15);
+	req.assert('senha', 'Senha é obrigatório').notEmpty();
 
 	var erros = req.validationErrors();
 
