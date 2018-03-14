8f7b982f0987102eec1d24fa6e79be51491ef369
william <willshinji@gmail.com>
2018-03-14 17:04:52 -0300

    logout user

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,15 +1,20 @@
 module.exports = function(application){
-	application.post('/chat', function(req, res){
-		application.app.controllers.chat.iniciaChat(application, req, res);
-	});
+  application.post('/chat', function(req, res){
+    application.app.controllers.chat.iniciaChat(application, req, res);
+  });
 
-	application.get('/chat', function(req, res){
-        if (req.session.autorizado==true){
-		application.app.controllers.chat.iniciaChat(application, req, res);
-          
-        }
-        else{
-          res.redirect("/")
-        }
-	});
+  application.get('/chat', function(req, res){
+    if (req.session.autorizado==true){
+      application.app.controllers.chat.iniciaChat(application, req, res);
+
+    }
+    else{
+      res.redirect("/")
+    }
+  });
+
+  application.get("/sair", function(req, res){
+    req.session.destroy();
+    res.redirect("/")
+  })
 }
