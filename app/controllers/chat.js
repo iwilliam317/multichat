f13b1c13777b259f4873b37a38f3d354612e68d9
william <willshinji@gmail.com>
2018-03-16 12:41:19 -0300

    added validation senha for at least 6 character

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -5,6 +5,7 @@ module.exports.iniciaChat = function(application, req, res){
 	req.assert('apelido', 'Nome ou apelido é obrigatório').notEmpty();
 	req.assert('apelido', 'Nome ou apelido deve conter entre 2 e 12 caracteres').len(2, 15);
 	req.assert('senha', 'Senha é obrigatório').notEmpty();
+	req.assert('senha', 'Senha deve conter ao menos 6 caracteres').len(6, 15);
 
 	var erros = req.validationErrors();
 
